05da44c68fc68aa26fdbeb9224e94ae1f8b49a63
badrman <bbadrman@gmail.com>
2023-09-10 22:47:30 +0100

    modif role super

--- src/Controller/ProspectController.php
+++ src/Controller/ProspectController.php
@@ -1,21 +1,21 @@
 <?php
 
 namespace App\Controller;
-  
+
 use App\Entity\Prospect;
 use App\Form\ProspectType;
 use App\Search\SearchProspect;
 use App\Form\ProspectAffectType;
 use App\Form\SearchProspectType;
 use App\Repository\ProspectRepository;
-use Doctrine\ORM\EntityManagerInterface; 
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route; 
+use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
@@ -24,7 +24,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
  * 
  */
 
- class ProspectController extends AbstractController
+class ProspectController extends AbstractController
 {
     private $entityManager;
     private $paginator;
@@ -41,10 +41,10 @@ use Symfony\Component\HttpFoundation\RequestStack;
      * @Route("/", name="app_prospect_index", methods={"GET", "POST"}) 
      */
     public function index(Request $request,  ProspectRepository $prospectRepository,  Security $security): Response
-    {  
-        
-        
-       
+    {
+
+
+
         $page = $request->query->getInt('page', 1);
         $perPage = 4;
 
@@ -52,35 +52,30 @@ use Symfony\Component\HttpFoundation\RequestStack;
         // $form->handleRequest($request);
         $form->handleRequest($this->requestStack->getCurrentRequest());
 
-            // Pour avoir tous les prospect en taut que je suis admin 
+        // Pour avoir tous les prospect en taut que je suis admin 
         $user = $security->getUser();
-        if (in_array('ROLE_SUPER_ADMIN', $user->getRoles(), true)) {
-           
+        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+
             // je recupere les prospects qui son pas encors affecter
             $prospect =  $prospectRepository->findByUserPasAffecter($this->paginator, $page, $perPage);
-           
-            
-            
         } else if (in_array('ROLE_TEAM', $user->getRoles(), true)) {
-          
+
             // je recupe seulement les prospects affecter au mon equipe
             $prospect =  $prospectRepository->findByUserChefEquipe($user);
-           
-            
         }
-      
+
 
         // Alors si je suis pas admin  je recupere selement les prospect attacher a moi 
         else {
-            
-            $prospect =  $prospectRepository->findByUserConect( $security->getUser()->getId());
-       
+
+            $prospect =  $prospectRepository->findByUserConect($security->getUser()->getId());
+
             // $request->getSession()->set('security', count($prospect) );
         }
 
         $this->requestStack->getSession()->set('security', count($prospect));
 
-      
+
         return $this->render('prospect/index.html.twig', [
             'prospects' => $prospect,
             'search_form' => $form->createView()
@@ -93,7 +88,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
     public function new(Request $request, ProspectRepository $prospectRepository): Response
     {
         $prospect = new Prospect();
-        $form = $this->createForm(ProspectType::class, $prospect );
+        $form = $this->createForm(ProspectType::class, $prospect);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -101,11 +96,11 @@ use Symfony\Component\HttpFoundation\RequestStack;
 
             $prospect->setAutor($this->getUser());
             $prospectRepository->add($prospect, true);
-           
+
             $this->addFlash('success', 'Votre Prospect a été ajouté avec succès!');
             return $this->redirectToRoute('app_prospect_index', [], Response::HTTP_SEE_OTHER);
         }
-     
+
         return $this->renderForm('prospect/new.html.twig', [
             'prospect' => $prospect,
             'form' => $form,
@@ -132,10 +127,9 @@ use Symfony\Component\HttpFoundation\RequestStack;
 
         if ($form->isSubmitted() && $form->isValid()) {
             $prospectRepository->add($prospect, true);
-            
+
             $this->addFlash('info', 'Votre Prospect a été affecté avec succès!');
             return $this->redirectToRoute('app_prospect_index', [], Response::HTTP_SEE_OTHER);
-
         }
 
         return $this->renderForm('partials/_show_modal.html.twig', [
@@ -143,7 +137,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
             'form' => $form,
         ]);
     }
-    
+
     /**
      * @Route("/{id}", name="app_prospect_delete", methods={"POST"})
      */
@@ -155,6 +149,4 @@ use Symfony\Component\HttpFoundation\RequestStack;
 
         return $this->redirectToRoute('app_prospect_index', [], Response::HTTP_SEE_OTHER);
     }
-
-     
 }

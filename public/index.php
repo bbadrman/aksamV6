f6d7a504d9fd26ed0c39bef5b49fad347934b6e7
badrman <bbadrman@gmail.com>
2024-09-11 11:55:23 +0100

    modif on idex.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
-use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpKernel\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+if (class_exists(Dotenv::class)) {
+    (new Dotenv())->loadEnv(dirname(__DIR__) . '/.env');
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
